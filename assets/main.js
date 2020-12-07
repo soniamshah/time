7b0d35c429b9896ac815830cdb01b2bd83c25141
soniamshah <72034453+soniamshah@users.noreply.github.com>
2020-12-07 18:44:52 -0500

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,29 @@
+$(function() {
+  var $clockText = $('#clock-text');
+  var $hour = $('#hour');
+  var $minute = $('#minute');
+  var $second = $('#second');
+  
+  function updateClock() {
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var ms = date.getMilliseconds();
+    var clockUpdateSpeed = 30; // speed in milliseconds
+
+ 	//$clockText.html(h + ":" + m + ":" + s + ":" + ms);
+// $hour.css('width:' + (vw/24 * h) + 'vw');
+//$minute.css('transform', 'rotate(' + (360/60 * m) + 'deg)');
+//$second.css('transform', 'rotate(' + (360/60 * s) + 'deg)');
+
+  $clockText.html(h + ":" + m + ":" + s + ":" + ms);
+ $hour.css('transform', 'rotate(' + (360/24 * h) + 'deg)');
+  $minute.css('transform', 'rotate(' + (360/60 * m) + 'deg)');
+  $second.css('transform', 'rotate(' + (360/60 * s) + 'deg)');
+
+    setTimeout(updateClock, clockUpdateSpeed);
+  }
+
+  updateClock();
+});
